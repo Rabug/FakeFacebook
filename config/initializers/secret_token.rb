c40b2576af2f25a758b1e978e633dfea22ee238d
Rabug <fraulmachin@gmail.com>
2012-04-14 14:02:06 +0100

    subo mi aplicacion de ruby Fakefacebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakeFacebook::Application.config.secret_token = '4a13527bb8f6213ab803a9e5a602f8d268cc0040c6eee8902b40d8e9c3ec1bf1ba235e7ce53049d4a518713b8f4b2e506ccc9eb4f7f315f242f900d800c67a3d'
